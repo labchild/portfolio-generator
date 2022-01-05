2b29111ecda7b28e632b932ce743776f7a42dbb3
l bates childs <labchilds@gmail.com>
2022-01-04 20:03:40 -0500

    added function to create html

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('====================');
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-}
+// learning about process.argv and other commands useful for node
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('====================');
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// }
 
+// printProfileData(profileDataArgs);
 
+const generatePage = (userName, githubName) => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio</title>
+    </head>
 
-printProfileData(profileDataArgs);
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+};
+
+console.log(name, github);
+console.log(generatePage(name, github));
