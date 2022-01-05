a7ef2df4afd857280db1110e643e9f4a1d8bbe10
l bates childs <labchilds@gmail.com>
2022-01-04 20:40:31 -0500

    added file writing functionality to create page from html

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
 const [name, github] = profileDataArgs;
 
-// learning about process.argv and other commands useful for node
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i++) {
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('====================');
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// }
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-// printProfileData(profileDataArgs);
-
-const generatePage = (userName, githubName) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio</title>
-    </head>
-
-    <body>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
-
-console.log(name, github);
-console.log(generatePage(name, github));
+    console.log('Portfolio complete! Check out index.html to see the output.')
+});
